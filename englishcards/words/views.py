cd7ecffcd12b2f25bf90f4f80c8470d8d47bbb69
Ziem <ziemowitpalka@gmail.com>
2023-01-02 16:01:32 +0100

    learn words added with favorites

--- englishcards/words/views.py
+++ englishcards/words/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from logins.models import User
 from django.contrib import messages
-from .models import MemoryCard, Quiz, QuizElement
+from .models import MemoryCard, Quiz, QuizElement, FavoriteUserCards
 from .forms import AddMemoryCardForm
+import random
 
 @login_required
 def addWordPage(request):
@@ -39,6 +40,47 @@ def addQuizPage(request):
     context = {'quiz_elements' : quiz_elements}
     return render(request, 'words/addQuiz.html', context)
 
+
+def selectLevel(request):
+    levels = ['A1','A2','B1','B2','C1','C2']
+    context = {'levels': levels}
+    return render(request, 'words/selectLevel.html', context)
+
+def learnByLevel(request, slug):
+    if slug == 'A1': level = 0
+    elif slug == 'A2': level = 1
+    elif slug == 'B1': level = 2
+    elif slug == 'B2': level = 3
+    elif slug == 'C1': level = 4
+    elif slug == 'C2': level = 5
+    words = list(MemoryCard.objects.filter(wordLevel = level))
+    userWords = FavoriteUserCards.objects.filter(user = request.user)
+    userWordsList = []
+    for word in userWords:
+        userWordsList.append(word.card)
+        
+    wordsToLearn = list(set(words) - set (userWordsList))
+    wordToLearn = []
+    if wordsToLearn:
+        wordToLearn = random.choice(wordsToLearn)
+    context = {'wordToLearn': wordToLearn}
+    
+    if request.method == 'POST':
+        if 'next' in request.POST:
+            pass
+        else:
+            counter = 1
+            for arg in request.POST:
+                if counter == 0:
+                    counter = 1
+                else:
+                    cardId = arg
+            card = MemoryCard.objects.get(id = cardId)
+            print(card)
+            favoriteCard = FavoriteUserCards.objects.create(user = request.user, card = card)
+            favoriteCard.save()
+    return render(request, 'words/learnByLevel.html', context)
+    
 
 # Create your views here.
 
